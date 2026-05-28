f8957d6a99421fa3f37f454240fbaebda7b498a7
Kleber Lucas <101598264+Kleberpointexe@users.noreply.github.com>
2026-05-28 17:00:16 -0300

    Delete loader.py

--- loader.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def load_data(paths):
-    ppi = pd.read_csv(paths["ppi"])
-    drug_targets = pd.read_csv(paths["drug_targets"])
-    cancer = pd.read_csv(paths["cancer"])
-    repo = pd.read_csv(paths["repo"])
-
-    return ppi, drug_targets, cancer, repo
